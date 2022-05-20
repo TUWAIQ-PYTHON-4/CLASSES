9867fb7fe9fa7743738024ee064173081dce7cf9
Abdulaziz Altariqi <AbdulazizAlturaiqi@gmail.com>
2022-05-21 01:33:36 +0300

    protected attrubites & fix some bugs

--- LAB13.py
+++ LAB13.py
@@ -7,72 +7,72 @@ class Vehicle:
 
     # properties brand, name, color, capacity, plate_number
     def __init__(self, brand: str, name: str, color: str, capacity: str, plate_number: str):
-        self.brand= brand
-        self.name = name
-        self.color = color
-        self.capacity = capacity
-        self.plate_number= plate_number
+        self.__brand= brand
+        self.__name = name
+        self.__color = color
+        self.__capacity = capacity
+        self.__plate_number= plate_number
 
     # methods
     def VechileInfo(self,): ## for print the detalis about the vechile
-        print("Brand:", self.brand, "|", "Name:", self.name,"|", "Color:", self.color,"|", "Capacity:", self.capacity,"|", "Plate N0:", self.plate_number)
+        print("Brand:", self.__brand, "|", "Name:", self.__name,"|", "Color:", self.__color,"|", "Capacity:", self.__capacity,"|", "Plate N0:", self.__plate_number)
 
     def drive(self,):
-        print(f"{self.name} is driving!")
+        print(f"{self.__name} is driving!")
 
     def drift(self):
-        print(f"{self.name} is drifting !!")  
+        print(f"{self.__name} is drifting !!")  
 
     def carry_cargo(self):
-        print(f"{self.name} is carrying cargo !!")  
+        print(f"{self.__name} is carrying cargo !!")  
 
     # for each of the properties do a setter & getter (encabsulate the data).
     def set_name(self, name):
-        self.name = name
+        self.__name = name
 
     def set_brand(self, brand):
-        self.brand = brand
+        self.__brand = brand
 
     def set_color(self, color):
-        self.color = color
+        self.__color = color
 
     def set_capacity(self, capacity):
-        self.capacity = capacity
+        self.__capacity = capacity
 
     def set_plate_number(self, plate_number):
-        self.plate_number = plate_number
+        self.__plate_number = plate_number
 
     def get_brand(self):
-        return self.brand
+        return self.__brand
 
     def get_name(self):
-        return self.name
+        return self.__name
 
     def get_color(self):
-        return self.color
+        return self.__color
 
     def get_capacity(self):
-        return self.capacity
+        return self.__capacity
 
     def get_plate_number(self):
-        return self.plate_number    
+        return self.__plate_number    
 
 # creating inheritance class from Vechile
 class bus(Vehicle):
 
     def drift(self):
-        print(f"{self.name} can not drift !!")  
+        print(f"{self.get_name()} can not drift !!")  
 
     def carry_cargo(self):
-        print(f"{self.name} is not used to carry cargo !!")      
+        print(f"{self.get_name()} is not used to carry cargo !!")      
 
 
 class truck(Vehicle):
     def drive(self,):
-        print(f"{self.name} is driving!")
+        print(f"{self.get_name()} is driving!")
 
     def drift(self,):
-        print(f"{self.name} can not drifting !!")    
+        print(f"{self.get_name()} can not drifting !!")    
 
 # Create the objects
 obj_bus = bus("TOYOTA", "Hiace", "White", "16 seats", "AAA 1111") 
